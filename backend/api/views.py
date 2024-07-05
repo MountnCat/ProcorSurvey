e526f8b9841d2df92acd1997e086cc92ae9947d6
Allen <allencabinta@gmail.com>
2024-07-05 12:38:29 +0800

    temporary frontend for login/logout

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -8,3 +10,33 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+def home(request):
+    return render(request, 'api/home.html')
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        
+        else:
+            messages.error(request, 'Username OR Password does not exist')
+        
+    context = {}
+    return render(request, 'api/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
+    path('', include('api.urls'))
 ]
 
